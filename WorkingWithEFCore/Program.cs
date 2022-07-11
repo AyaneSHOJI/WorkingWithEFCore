0c2854e662cf3aa99821d206d8704bce27ab8a35
AyaneSHOJI <shojiayane@gmail.com>
2022-07-11 11:56:45 +0200

    filter with Include

--- WorkingWithEFCore/Program.cs
+++ WorkingWithEFCore/Program.cs
@@ -4,6 +4,7 @@ using static System.Console;
 
 Console.WriteLine($"Using {ProjectConstants.DatabaseProvider} database provider");
 queryingCategories();
+FilteredInclude();
 
 static void queryingCategories()
 {
@@ -24,6 +25,37 @@ static void queryingCategories()
         {
             WriteLine($"{c.CategoryName} has {c.Products.Count} products");
         }
+
+    }
+}
+
+static void FilteredInclude()
+{
+    using (Northwind db = new())
+    {
+        Write("Enter a minimum for units in stock: ");
+        string unitsInStock = ReadLine() ?? "10";
+        int stock = int.Parse(unitsInStock);
+
+        IQueryable<Category>? categories = db.Categories?.Include(c => c.Products.Where(p => p.Stock >= stock));
+
+        if(categories is null)
+        {
+            WriteLine("No categories found.");
+            return;
+        }
+
+        foreach(Category c in categories)
+        {
+            WriteLine($"{c.CategoryName} has {c.Products.Count} products with a minimum of {stock} units in stock");
+            
+            foreach(Product p in c.Products)
+            {
+                WriteLine($"   {p.ProductName} has {p.Stock} units in stock.");
+            }
+
+        }
+
 
     }
 }
